c4c649b21ecb07d6062a3123d97794df9783974a
rahulgidde <rahulgidde1212@gmail.com>
2020-02-14 17:13:05 +0530

    ADDED:Check Employee Attendance

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,3 +2,17 @@
 
 echo "------------------WELCOME------------------"
 
+#CONSTANT
+Is_present=1
+
+#STORE RANDOM VALUE
+randomcheck=$((RANDOM%2))
+
+#CHECK EMPLOYEE IS PRESENT OR ABSENT
+if [ $randomcheck -eq $Is_present ]
+then
+	echo "Employee Is Present"
+else
+	echo "Employee Is Absent"
+fi
+
