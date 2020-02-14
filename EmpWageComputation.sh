c4ce4040ebf3d29fd5cd89828944b2a160c0f3cc
rahulgidde <rahulgidde1212@gmail.com>
2020-02-14 17:55:17 +0530

    ADDED:Employee Daily Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,18 +1,22 @@
 #!/bin/bash -x
 
-echo "------------------WELCOME------------------"
-
 #CONSTANT
 IS_PRESENT=1
+PER_DAY_WAGE=20
 
 #STORE RANDOM VALUE
 randomcheck=$((RANDOM%2))
 
-#CHECK EMPLOYEE IS PRESENT OR ABSENT
+#CALCULATING EMPLOYEE DAILY WAGE
 if [ $randomcheck -eq $IS_PRESENT ]
 then
 	echo "Employee Is Present"
+   Present_hour=8
 else
 	echo "Employee Is Absent"
+   Present_hour=0
 fi
 
+#CHECKING EMPLOYEE PER DAY WAGE
+dailywage=$(($PER_DAY_WAGE*$Present_hour))
+echo "Employee Per Day Wage Is: $dailywage Rs."
