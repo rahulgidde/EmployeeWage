ac60f9cbe34857803afa14deb3474b32c783756f
rahulgidde <rahulgidde1212@gmail.com>
2020-02-15 12:36:13 +0530

    REFACTOR:Replace forloop with while loop and Calculate Employee Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -7,12 +7,13 @@ PART_TIME_HOURS=4
 IS_FULL_TIME=1
 Is_PART_TIME=2
 DAYS_PER_MONTH=20
+TOTALL_WORKING_HOURS=100
 
 #VARIABLE
 employeeHours=0
 
-#CHECK EMPLOYEE IS PRESENT OR ABSENT
-for (( index=1; index<=$DAYS_PER_MONTH; index++ ))
+#CHECK EMPLOYEE IS PRESENT OR ABSENT TILL CONDITION IS NOT SATISFIED
+while [ $DAYS_PER_MONTH -ne 0 -a $employeeHours -le $TOTALL_WORKING_HOURS ]
 do
 	randomcheck=$((RANDOM%3))
 	case $randomcheck in
@@ -26,8 +27,9 @@ do
 			employeeHours=$(($employeeHours+0))
 			;;
 	esac
+			((DAYS_PER_MONTH--))
 done
 
-#CALCULATING EMPLOYEE WAGE PER MONTH
+#CALCULATE EMPLOYEE WAGE PER MONTH
 wagePerMonth=$(($PER_HOUR_WAGE*$employeeHours))
 echo "Employee Wage Per Month = $wagePerMonth"
